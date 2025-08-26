6117248bed7662766ef1ba76fb3285aebc1685bc
BalajiBhure <balajibhure3@gmail.com>
2025-08-26 22:24:13 +0530

    Delete vehicleparking directory

--- vehicleparking/includes/dbconn.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con=mysqli_connect("localhost", "root", "", "VehicleParkingDB");
-    if(mysqli_connect_errno()){
-    echo "Connection Fail".mysqli_connect_error();
-    }
-  ?>
--- vehicleparking/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-	<div class="col-sm-12">
-		<p class="back-link">&copy; <?php echo date("Y"); ?> - Developed By Balaji Bhure </p>
-	</div>
-</footer>
